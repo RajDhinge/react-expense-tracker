57bebc92b08246a613d26c9f3c4b10c0047c4b6a
Rajkumar Dhinge <rajkumar.dhinge@wisdmlabs.com>
2023-05-10 04:27:53 +0530

    :zap: removing unwanted buttons

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,21 +4,15 @@ import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 
 const ExpenseItem = (props) => {
-	const [title, setTitle] = useState(props.title);
-
-	const titleHandler = () => {
-		setTitle("Updating title");
-	};
 
 	return (
 		<Card className='expense-item'>
 			<ExpenseDate
 				date={props.date} />
 			<div className='expense-item__description'>
-				<h2>{title}</h2>
+				<h2>{props.title}</h2>
 				<div className='expense-item__price'>${props.price}</div>
 			</div>
-			<button onClick={titleHandler}>Change title</button>
 		</Card>
 	);
 }
