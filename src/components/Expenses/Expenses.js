7f7e04999ca15ddd012423c98bea26952aae77d0
Rajkumar Dhinge <rajkumar.dhinge@wisdmlabs.com>
2023-05-10 03:26:33 +0530

    :zap: Added year filter used two way data passing and upward state lifting

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,17 +1,29 @@
-import ExpenseItem from './ExpenseItem'
+import React, { useState } from 'react';
+import ExpenseItem from './ExpenseItem';
+import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
 
 const Expenses = (props) => {
+	const [filterYearValue, setFilterYearValue] = useState('2021');
+
+	const setFilterExpensesYear = (setYearvalue) => {
+		setFilterYearValue(setYearvalue);
+		console.log(setYearvalue);
+	}
 	return (
-		props.expenses.map((expense) => {
-			return (
-				<ExpenseItem
-					key={expense.id}
-					title={expense.title}
-					price={expense.amount}
-					date={expense.date} />
-			)
-		})
+		<Card>
+			<ExpensesFilter selected={filterYearValue} onFilterSet={setFilterExpensesYear} />
+			{props.expenses.map((expense) => {
+				return (
+					<ExpenseItem
+						key={expense.id}
+						title={expense.title}
+						price={expense.amount}
+						date={expense.date} />
+				)
+			})}
+		</Card>
 	);
-}
+};
 
 export default Expenses;
