047a13305cc0d111e4d92b318083180a82ddc6ea
Rajkumar Dhinge <rajkumar.dhinge@wisdmlabs.com>
2023-05-10 16:59:47 +0530

    :zap: Added bar chart to dynamically show the expenses.

---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,26 @@
+import ChartBar from './ChartBar'
+import './Chart.css'
+
+const Chart = (props) => {
+	const dataPointsValue = props.dataPoints.map(
+		dataPoints => dataPoints.value
+	);
+
+	const totalMaximum = Math.max(...dataPointsValue);
+	return (
+		<div className='chart'>
+			{props.dataPoints.map(
+				(dataPoints) => (
+					<ChartBar
+						key={dataPoints.label}
+						value={dataPoints.value}
+						maxValue={totalMaximum}
+						label={dataPoints.label}
+					/>
+				)
+			)}
+		</div>
+	);
+}
+
+export default Chart;
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
 import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
 
 const Expenses = (props) => {
 	const [filterYearValue, setFilterYearValue] = useState('2021');
@@ -16,6 +17,7 @@ const Expenses = (props) => {
 
 	return (
 		<Card>
+			<ExpensesChart expenses={filteredExpensesByYear} />
 			<ExpensesFilter selected={filterYearValue} onFilterSet={setFilterExpensesYear} />
 			<ExpensesList expenses={filteredExpensesByYear} />
 		</Card>
